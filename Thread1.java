5beb803eab4b16925d0a736312e694e918ea5a40
rahmthullakhaja <83999868+rahmthullakhaja@users.noreply.github.com>
2021-05-21 09:15:39 +0530

    Add files via upload

--- Thread1.java
+++ Thread1.java
@@ -1,4 +1,16 @@
 
-public class Thread1 {
-
+public class Thread1  extends Thread{
+public  void run()
+{
+for(int i=0;i<5;i++)
+{
+System.out.println(i);
 }
+}
+public static void main(String args[])
+{
+Thread1 t1=new Thread1();
+t1.start();
+}
+}
+
